1881791466acd1d03a3e0eb4eca19cce3227c6b4
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-12 10:17:44 -0400

    commit tree adf958a7b18e25461680aa8643748f80ccb086ed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(value)
+        @breed = value
+    end
+
+    def breed
+        @breed
+    end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
 
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
+
+    def job=(value)
+        @job = value
+    end
+
+    def job
+        @job
+    end
+
 end
